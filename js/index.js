d6e7bd76ec80fae47d70c13c4815f237b7649c68
Luis LT <105554066+LTprograms@users.noreply.github.com>
2025-01-09 18:18:05 -0500

    vista slider

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("load", function () {
+    var swiper = new Swiper(".mySwiper", {
+        effect: "cube",
+        grabCursor: true,
+        cubeEffect: {
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+    });
+}, true)
